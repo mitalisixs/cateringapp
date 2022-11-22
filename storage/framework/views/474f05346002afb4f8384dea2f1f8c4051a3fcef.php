cee915b3491ecf48315e3ce5f8de3064e523c05a
Mitali <mmehta@techinsurgence.com>
2022-11-22 19:43:55 +0530

    Initial Commit

---
+++ storage/framework/views/474f05346002afb4f8384dea2f1f8c4051a3fcef.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <?php echo $__env->make('layouts.footers.nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</footer><?php /**PATH C:\wamp64\www\CategoringManagement\resources\views/layouts/footers/auth.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e45e0cdf4eb427b945c456b0fdc53f92599430c6.php
@@ -0,0 +1,2 @@
+<div class="row align-items-center justify-content-xl-between">
+</div><?php /**PATH C:\wamp64\www\CategoringManagement\resources\views/layouts/footers/nav.blade.php ENDPATH**/ ?>
