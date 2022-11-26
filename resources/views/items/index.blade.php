6503afc363e68daff41cd1464df1c1654e6934f1
Mitali <mmehta@techinsurgence.com>
2022-11-26 17:22:04 +0530

    Category Product Display

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -154,51 +154,98 @@
                     @if($category->active == 1)
                         <div  style="margin-left:40px">
                         @foreach ( $subcategories as $subcategory)
-                        <div class="alert alert-warning">
-                            <div class="row">
-                                <div class="col">
-                                    <span class="h2 font-weight-bold mb-0 text-white">{{ $subcategory->name }}</span>
-                                </div>
-                                <div class="col-auto">
-                                    <div class="row">
-                                        @if($canAdd)
-                                        <button class="btn btn-icon btn-1 btn-sm btn-primary" type="button" data-toggle="modal" data-target="#modal-new-item" data-toggle="tooltip" data-placement="top" title="{{ __('Add item') }} in {{$subcategory->name}}" onClick=(setSelectedCategoryId({{ $subcategory->id }}))>
-                                            <span class="btn-inner--icon"><i class="fa fa-plus"></i></span>
-                                        </button>
-                                        @else
-                                        <a href="{{ route('plans.current')}}" class="btn btn-icon btn-1 btn-sm btn-warning" type="button">
-                                            <span class="btn-inner--icon"><i class="fa fa-plus"></i> {{ __('Menu size limit reaced') }}</span>
-                                        </a>
-                                        @endif
-                                        <button class="btn btn-icon btn-1 btn-sm btn-warning" type="button" id="edit" data-toggle="modal" data-target="#modal-edit-category" data-toggle="tooltip" data-placement="top" title="{{ __('Edit Sub category') }} {{ $subcategory->name }}" data-id="<?= $subcategory->id ?>" data-name="<?= $subcategory->name ?>">
-                                            <span class="btn-inner--icon"><i class="fa fa-edit"></i></span>
-                                        </button>
-
-
-
-                                        <form action="{{ route('categories.destroy', $subcategory) }}" method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <button class="btn btn-icon btn-1 btn-sm btn-danger" type="button" onclick="confirm('{{ __("Are you sure you want to delete this category?") }}') ? this.parentElement.submit() : ''" data-toggle="tooltip" data-placement="top" title="{{ __('Delete') }} {{$category->name}}">
-                                                <span class="btn-inner--icon"><i class="fa fa-trash"></i></span>
+                            <div class="alert alert-warning">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="h2 font-weight-bold mb-0 text-white">{{ $subcategory->name }}</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="row">
+                                            @if($canAdd)
+                                            <button class="btn btn-icon btn-1 btn-sm btn-primary" type="button" data-toggle="modal" data-target="#modal-new-item" data-toggle="tooltip" data-placement="top" title="{{ __('Add item') }} in {{$subcategory->name}}" onClick=(setSelectedCategoryId({{ $subcategory->id }}))>
+                                                <span class="btn-inner--icon"><i class="fa fa-plus"></i></span>
                                             </button>
-                                        </form>
+                                            @else
+                                            <a href="{{ route('plans.current')}}" class="btn btn-icon btn-1 btn-sm btn-warning" type="button">
+                                                <span class="btn-inner--icon"><i class="fa fa-plus"></i> {{ __('Menu size limit reaced') }}</span>
+                                            </a>
+                                            @endif
+                                            <button class="btn btn-icon btn-1 btn-sm btn-warning" type="button" id="edit" data-toggle="modal" data-target="#modal-edit-category" data-toggle="tooltip" data-placement="top" title="{{ __('Edit Sub category') }} {{ $subcategory->name }}" data-id="<?= $subcategory->id ?>" data-name="<?= $subcategory->name ?>">
+                                                <span class="btn-inner--icon"><i class="fa fa-edit"></i></span>
+                                            </button>
 
 
 
+                                            <form action="{{ route('categories.destroy', $subcategory) }}" method="post">
+                                                @csrf
+                                                @method('delete')
+                                                <button class="btn btn-icon btn-1 btn-sm btn-danger" type="button" onclick="confirm('{{ __("Are you sure you want to delete this category?") }}') ? this.parentElement.submit() : ''" data-toggle="tooltip" data-placement="top" title="{{ __('Delete') }} {{$category->name}}">
+                                                    <span class="btn-inner--icon"><i class="fa fa-trash"></i></span>
+                                                </button>
+                                            </form>
+
+
+
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div class="row justify-content-center">
+                                <div class="col-lg-12">
+                                    <div class="row row-grid">
+
+                                        @foreach ( $subcategory->items as $item)
+                                        <div class="col-lg-3">
+                                            <a href="{{ route('items.edit', $item) }}">
+                                                <div class="card">
+                                                    <!-- <img class="card-img-top" src="{{  asset('/').$item->logom }}" alt="..."> -->
+                                                    <div class="card-body">
+                                                        <h3 class="card-title text-primary text-uppercase">{{ $item->name }}</h3>
+                                                        <p class="card-text description mt-3">{{ $item->description }}</p>
+
+                                                        <!-- <span class="badge badge-primary badge-pill">@money($item->price, config('settings.cashier_currency'),config('settings.do_convertion'))</span>
+
+                                                        <p class="mt-3 mb-0 text-sm">
+                                                            @if($item->available == 1)
+                                                            <span class="text-success mr-2">{{ __("AVAILABLE") }}</span>
+                                                            @else
+                                                            <span class="text-danger mr-2">{{ __("UNAVAILABLE") }}</span>
+                                                            @endif
+                                                        </p> -->
+                                                    </div>
+                                                </div>
+                                                <br />
+                                            </a>
+                                        </div>
+                                        @endforeach
+
+                                        @if($canAdd)
+                                        <div class="col-lg-3">
+                                            <a data-toggle="modal" data-target="#modal-new-item" data-toggle="tooltip" data-placement="top" href="javascript:void(0);" onclick=(setSelectedCategoryId({{ $subcategory->id }}))>
+                                                <div class="card">
+                                                    <img class="card-img-top" src="{{ asset('images') }}/default/add_new_item.jpg" alt="...">
+                                                    <div class="card-body">
+                                                        <h3 class="card-title text-primary text-uppercase">{{ __('Add item') }}</h3>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <br />
+                                        </div>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
                         </div>
                         <div class="row justify-content-center">
                             <div class="col-lg-12">
                                 <div class="row row-grid">
 
-                                    @foreach ( $subcategory->items as $item)
+                                    @foreach ( $category->items as $item)
                                     <div class="col-lg-3">
                                         <a href="{{ route('items.edit', $item) }}">
                                             <div class="card">
-                                                <img class="card-img-top" src="{{  asset('/').$item->logom }}" alt="...">
+                                                <!-- <img class="card-img-top" src="{{  asset('/').$item->logom }}" alt="..."> -->
                                                 <div class="card-body">
                                                     <h3 class="card-title text-primary text-uppercase">{{ $item->name }}</h3>
                                                     <p class="card-text description mt-3">{{ $item->description }}</p>
@@ -221,7 +268,7 @@
 
                                     @if($canAdd)
                                     <div class="col-lg-3">
-                                        <a data-toggle="modal" data-target="#modal-new-item" data-toggle="tooltip" data-placement="top" href="javascript:void(0);" onclick=(setSelectedCategoryId({{ $subcategory->id }}))>
+                                        <a data-toggle="modal" data-target="#modal-new-item" data-toggle="tooltip" data-placement="top" href="javascript:void(0);" onclick=(setSelectedCategoryId({{ $category->id }}))>
                                             <div class="card">
                                                 <img class="card-img-top" src="{{ asset('images') }}/default/add_new_item.jpg" alt="...">
                                                 <div class="card-body">
@@ -234,8 +281,6 @@
                                     @endif
                                 </div>
                             </div>
-                        </div>
-                        @endforeach
                         </div>
                     @endif
 
