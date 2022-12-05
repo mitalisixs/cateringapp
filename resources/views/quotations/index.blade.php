f053950bd479bc9c9dff1e4b99b0c60804ab8ff5
Mitali <mmehta@techinsurgence.com>
2022-12-05 18:51:29 +0530

    .gitignore is now working

--- resources/views/quotations/index.blade.php
+++ resources/views/quotations/index.blade.php
@@ -1,22 +1,37 @@
 @extends('layouts.app', ['title' => __('Quotations')])
 @section('admin_title')
-    {{__('Quotations')}}
+{{__('Quotations')}}
 @endsection
 @section('content')
-    <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
-    </div>
+<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="row align-items-center py-4">
+                <!--<div class="col-lg-6 col-7">
+                </div>-->
+                <div class="col-lg-12 col-12 text-right">
+
+                    <a class="btn btn-sm btn-warning" href="{{route('quotations.create')}}">
+                        <span class="btn-inner--icon"><i class="fa fa-plus"></i> {{ __('Add new Quotation') }}</span>
+                    </a>
 
 
-    <div class="container-fluid mt--7">
-        <div class="row">
-            <div class="col">
-                <!-- Order Card -->
-                @include('quotations.partials.ordercard')
+
+                </div>
             </div>
         </div>
-        @include('layouts.footers.auth')
-        @include('quotations.partials.modals')
     </div>
+</div>
+
+
+<div class="container-fluid mt--7">
+    <div class="row">
+        <div class="col">
+            <!-- Order Card -->
+            @include('quotations.partials.ordercard')
+        </div>
+    </div>
+    @include('layouts.footers.auth')
+    @include('quotations.partials.modals')
+</div>
 @endsection
-
-
--- storage/framework/views/e45e0cdf4eb427b945c456b0fdc53f92599430c6.php
+++
@@ -1,2 +0,0 @@
-<div class="row align-items-center justify-content-xl-between">
-</div><?php /**PATH C:\wamp64\www\CategoringManagement\resources\views/layouts/footers/nav.blade.php ENDPATH**/ ?>
